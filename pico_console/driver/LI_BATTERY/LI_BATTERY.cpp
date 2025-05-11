f4d96c7a35dfbda18a78c783a50375f5ce3223d9
Crem2y <31000852+Crem2y@users.noreply.github.com>
2025-05-11 15:05:35 +0900

    Update battery calculation formula

--- pico_console/driver/LI_BATTERY/LI_BATTERY.cpp
+++ pico_console/driver/LI_BATTERY/LI_BATTERY.cpp
@@ -2,10 +2,6 @@
 
 #include "LI_BATTERY.hpp"
 
-double map_double(double x, double in_min, double in_max, double out_min, double out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 LI_BATTERY::LI_BATTERY(int adc_pin, double res_ratio) {
   _adc_pin = adc_pin;
   _res_ratio = res_ratio;
@@ -24,12 +20,28 @@ double LI_BATTERY::get_voltage(void) {
     adc_raw += adc_read();
   }
 
-  //return (((double)adc_raw * (3.3/4096)) / _res_ratio);
-  voltage = (double)adc_raw * (3.3/65536) * 2 + 0.150;
+  voltage = (double)adc_raw * (3.3/65536) / _res_ratio;
   return voltage;
 }
 
 double LI_BATTERY::get_level(void) {
-  level = map_double(get_voltage(), BAT_V_MIN, BAT_V_MAX, 0.0, 100.0);
+  double bat_voltage = get_voltage();
+
+  double bat_level = 0;
+  if(bat_voltage >= 4.2) {
+    bat_level = 100;
+  } else if(bat_voltage > 4.1) { // 99.9 ~ 96.0, 100mV
+    bat_level = (((bat_voltage - 4.1) / 0.1) * 4) + 96;
+  } else if(bat_voltage > 3.8) { // 95.9 ~ 56.0, 300mV
+    bat_level = (((bat_voltage - 3.8) / 0.3) * 40) + 56;
+  } else if(bat_voltage > 3.4) { // 55.9 ~ 13.0, 400mV
+    bat_level = (((bat_voltage - 3.4) / 0.4) * 43) + 13;
+  } else if(bat_voltage > 3.0) { // 12.9 ~ 0.0, 400mV
+    bat_level = (((bat_voltage - 3.0) / 0.4) * 13) + 0;
+  } else {
+    bat_level = 0;
+  }
+  level = bat_level;
+
   return level;
 }
